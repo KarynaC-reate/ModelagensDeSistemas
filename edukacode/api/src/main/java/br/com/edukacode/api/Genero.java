159e373381ac7452faf0951dbd99abd843199f50
KarynaC-reate <k4ahcarvalho@gmail.com>
2025-08-28 17:19:53 -0300

    Avaliação - Classe Gênero; UML e MER
     Classe Gênero; UML e MER

--- edukacode/api/src/main/java/br/com/edukacode/api/Genero.java
+++ edukacode/api/src/main/java/br/com/edukacode/api/Genero.java
@@ -5,5 +5,40 @@ public class Genero {
     private Long id;
     private String nome;
 
-    
+    //Construtor vazio
+    public Genero() {
+    }
+
+    //Construtor
+    public Genero(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    //Getter
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    //Setter
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Genero{" +
+               "id=" + id +
+               ", nome='" + nome + '\'' +
+               '}';
+    }
 }
+    
